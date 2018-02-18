e863bd2d73c34c063eb5d889df55602fdc843a65
Simen Krantz <simenkrantzknudsen@gmail.com>
2018-02-18 15:27:51 +0100

    Functions for light in order buttons I

--- ownfuncs.c
+++ ownfuncs.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-
 #include "ownfuncs.h"
 
-void initialize(void)
+void
+initialize(void)
 {
     printf("Press STOP button to stop elevator and exit program.\n");
 
@@ -21,7 +21,9 @@ void initialize(void)
     }
 }
 
-void set_floor_lights(void)
+
+void
+set_floor_lights(void)
 {
         int current_floor = elev_get_floor_sensor_signal();
         if (current_floor != -1){
@@ -29,7 +31,9 @@ void set_floor_lights(void)
         }
 }
 
-void time_delay(int millisecs)
+
+void
+time_delay(int millisecs)
 {
 	long pause;
 	clock_t now, then;
@@ -40,7 +44,9 @@ void time_delay(int millisecs)
 	}
 }
 
-void change_of_motor_direction(void)
+
+void
+change_of_motor_direction(void)
 {
 	// Change direction when we reach top/bottom floor
         if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
@@ -53,49 +59,105 @@ void change_of_motor_direction(void)
 }
 
 
-
-/**
-Mulig de to neste bør slås sammen til en? 
-0 kan være default, negative 1-4 for ned og positive 1-4 for opp?
-*/
-
-int up_button_outside_pressed(void)
+int
+button_outside_pressed(void)
 {
-	if (elev_get_button_signal(BUTTON_CALL_UP, 0))		//Up from 1st floor
-		return 0;
-	else if (elev_get_button_signal(BUTTON_CALL_UP, 1))	//Up from 2nd floor
+	if (elev_get_button_signal(BUTTON_CALL_UP, 0))			// UP from 1st floor
 		return 1;
-	else if (elev_get_button_signal(BUTTON_CALL_UP, 2))	//Up from 3rd floor
+	else if (elev_get_button_signal(BUTTON_CALL_UP, 1))		// UP from 2nd floor
 		return 2;
+	else if (elev_get_button_signal(BUTTON_CALL_UP, 2))		// UP from 3rd floor
+		return 3;
+
+	else if (elev_get_button_signal(BUTTON_CALL_DOWN, 1))	// DOWN from 2nd floor
+		return -1;
+	else if (elev_get_button_signal(BUTTON_CALL_DOWN, 2))	// DOWN from 3rd floor
+		return -2;
+	else if (elev_get_button_signal(BUTTON_CALL_DOWN, 3))	// DOWN from 4th floor
+		return -3;
 	else
-		return -1;	//Default
+		return 0;	// Default
 }
 
-int down_button_outside_pressed(void)
+
+int
+order_button_inside_pressed(void)
 {
-	if (elev_get_button_signal(BUTTON_CALL_DOWN, 1))			//Down from 2nd floor
+	if (elev_get_button_signal(BUTTON_COMMAND, 0))		// Order button 1st floor pressed
+		return 0;
+	else if (elev_get_button_signal(BUTTON_COMMAND, 1))	// Order button 2nd floor pressed
 		return 1;
-	else if (elev_get_button_signal(BUTTON_CALL_DOWN, 2))	//Down from 3rd floor
+	else if (elev_get_button_signal(BUTTON_COMMAND, 2))	// Order button 3rd floor pressed
 		return 2;
-	else if (elev_get_button_signal(BUTTON_CALL_DOWN, 3))	//Down from 4th floor
+	else if (elev_get_button_signal(BUTTON_COMMAND, 3))	// Order button 4th floor pressed
 		return 3;
 	else
-		return -1;	//Default
+		return -1;	// Default
 }
 
 
-
-int order_button_inside_pressed(void)
+// button_outside_pressed() gives the int outside_button_pressed in main()
+void
+set_outside_order_lights(int outside_button_pressed, int value)
 {
-	if (elev_get_button_signal(BUTTON_COMMAND, 0))		//Order button 1st floor pressed
-		return 0;
-	else if (elev_get_button_signal(BUTTON_COMMAND, 1))	//Order button 2nd floor pressed
-		return 1;
-	else if (elev_get_button_signal(BUTTON_COMMAND, 2))	//Order button 3rd floor pressed
-		return 2;
-	else if (elev_get_button_signal(BUTTON_COMMAND, 3))	//Order button 4th floor pressed
-		return 3;
-	else
-		return -1;	//Default
+	// Turns light off
+	if(value == 0) {
+
+	}
+
+	// Turns light on
+	else {
+		switch(outside_button_pressed) {
+			case 1:
+				elev_set_button_lamp(BUTTON_CALL_UP, 0, 1);	// Light in 1st floor UP button
+				break;
+			case 2:
+				elev_set_button_lamp(BUTTON_CALL_UP, 1, 1);	// Light in 2nd floor UP button
+				break;
+			case 3:
+				elev_set_button_lamp(BUTTON_CALL_UP, 2, 1);	// Light in 3rd floor UP button
+				break;
+
+			case -1:
+				elev_set_button_lamp(BUTTON_CALL_DOWN, 1, 1); // Light in 2nd floor DOWN button
+				break;
+			case -2:
+				elev_set_button_lamp(BUTTON_CALL_DOWN, 2, 1); // Light in 3rd floor DOWN button
+				break;
+			case -3:
+				elev_set_button_lamp(BUTTON_CALL_DOWN, 3, 1); // Light in 4th floor DOWN button
+				break;
+		}
+	}
 }
 
+
+// order_button_inside_pressed() gives the int floor_order_button in main()
+void
+set_floor_order_lights(int floor_order_button, int value)
+{
+	// Turns light off
+	if (value == 0) {
+		switch(floor_order_button) {
+			
+		}
+	}
+
+	// Turns light on
+	else {
+		switch(floor_order_button) {
+			case 0:
+				elev_set_button_lamp(BUTTON_COMMAND, 0, 1);
+				break;
+			case 1:
+				elev_set_button_lamp(BUTTON_COMMAND, 1, 1);
+				break;
+			case 2:
+				elev_set_button_lamp(BUTTON_COMMAND, 2, 1);
+				break;
+			case 3:
+				elev_set_button_lamp(BUTTON_COMMAND, 3, 1);
+				break;
+		}
+	}
+}
--- ownfuncs.h
+++ ownfuncs.h
@@ -6,41 +6,56 @@ Compressed initialize function to have a more
 straightforward main function.
 Line 12-29 in the original main.c from BB.
 */
-void initialize(void);
+void
+initialize(void);
 
 
-//Sets the floorlight for the last passed floor
-void set_floor_lights(void);
+//Sets the floorlight for the last passed floor.
+void
+set_floor_lights(void);
 
 
-//Delays action for a given number of milliseconds
-void time_delay(int millisecs);
+//Delays action for a given number of milliseconds.
+void
+time_delay(int millisecs);
 
 
-//Turn direction at endpoints and set floor indicators
-void change_of_motor_direction(void);
+//Turn direction at endpoints and set floor indicators.
+void
+change_of_motor_direction(void);
 
 
 /**
-Returns -1 if no up order button is pressed.
-Returns 0 for 1st floor, 1 for 2nd floor, 2 for 3rd floor.
+Returns 0 if no order button is pressed.
+BUTTON_CALL_UP returns positive values. 1 for 1st floor, 2 for 2nd floor, 3 for 3rd floor.
+BUTTON_CALL_DOWN returns negative values. -1 for 2nd floor, -2 for 3rd floor, -3 for 4th floor. 
 */
-int up_button_outside_pressed(void);
-
-/**
-Returns -1 if no down order button is pressed.
-Returns 1 for 2nd floor, 2 for 3rd floor, 3 for 4th floor.
-*/
-int down_button_outside_pressed(void);
-
+int
+button_outside_pressed(void);
 
 /**
 Returns -1 if no order button inside the elevator is pressed.
 Returns 0 for 1st floor, 1 for 2nd floor, 2 for 3rd floor, 3 for 4th floor.
 */
-int order_button_inside_pressed(void);
+int
+order_button_inside_pressed(void);
 
+/**
+Sets the lights in the up/down buttons
+outside the elevator.
+@param o_b_p ranges from negative 3 to positive 3, given by button_outside_pressed()
+@param value turns on or off the given light. 0 turns off, nonzero turns on.
+*/
+void
+set_outside_order_lights(int outside_button_pressed, int value);
 
+/**
+Sets the order lights inside the elevator (1st - 4th floor buttons).
+@param f_o_b is given by order_button_inside_pressed().
+@param value turns on or off the given light. 0 turns off, nonzero turns on.
+*/
+void
+set_floor_order_lights(int floor_order_button, int value);
 
 
 #endif
