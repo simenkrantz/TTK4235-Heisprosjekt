10f7310735d09a65058f423101f17978c900e5dd
CD-cover linje <Ukjent venninne>
2018-03-10 13:53:31 +0100

    Last change, delivering

--- emergency_stop.c
+++ emergency_stop.c
@@ -13,26 +13,31 @@ stop_state(int array[4][3], int* motor_dir)
 	if(elev_get_floor_sensor_signal() != -1 && elev_get_stop_signal()) {
 
 		elev_set_motor_direction(DIRN_STOP);
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 3; j++) {
+                array[i][j] = 0;
+            }
+            turn_off_button_lights(i);
+        }
 
 		while(elev_get_stop_signal()) {
 			elev_set_stop_lamp(1);
-			continue;
+            continue;
 		}
 
 		elev_set_stop_lamp(0);
 		open_close_door(array);
-
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 3; j++) {
-                array[i][j] = 0;
-        	}
-        	turn_off_button_lights(i);
-        }
 	}
 
 	else if(elev_get_stop_signal()) {
 
 		elev_set_motor_direction(DIRN_STOP);
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 3; j++) {
+                array[i][j] = 0;
+            }
+            turn_off_button_lights(i);
+        }
 
 		while(elev_get_stop_signal()) {
 			elev_set_stop_lamp(1);
@@ -40,13 +45,6 @@ stop_state(int array[4][3], int* motor_dir)
 		}
 
 		elev_set_stop_lamp(0);
-
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 3; j++) {
-                array[i][j] = 0;
-        	}
-        	turn_off_button_lights(i);
-        }
     }
 }
 
